d7957352801bf70abe2a9796bf89474a46b8f37f
“Anais <ananavelez@gmail.com>
2017-06-13 10:49:03 -0400

    casi liso

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+	var input = document.getElementById("input-suscribir").value;
+	var boton= document.getElementById("boton-suscri");
+  
+  boton.addEventListener("click",function(){
+  	if (input.length===0){
+		alert("escribe tu correo")
+	}else{
+      alert("bien")
+	}
+  });
